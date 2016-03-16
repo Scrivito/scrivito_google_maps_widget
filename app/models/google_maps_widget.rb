b13b7ed36300395041ae609ac6a4e8cd039d6e49
Gert Geidel <gert.geidel@infopark.de>
2016-03-16 10:18:13 +0100

    Fix for no zoom value for old widgets

--- app/models/google_maps_widget.rb
+++ app/models/google_maps_widget.rb
@@ -2,4 +2,8 @@ class GoogleMapsWidget < Widget
   attribute :address, :string
   attribute :height, :string, default: '50vh'
   attribute :zoom, :enum, values: (0..21).to_a.map {|e| e.to_s}, default: '13'
+
+  def zoom_level
+    zoom || 13
+  end
 end
